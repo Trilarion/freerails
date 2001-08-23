43acc812e976fca1b9a912b0d8303ff6e9d98a72
bartcv <>
2001-08-23 19:01:27 +0000

    Moved GUI to GameApplication

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -6,8 +6,11 @@
 #include "i18n.h"
 #include "GameMainWindow.h"
 #include "GameModeSelectDialog.h"
+#include "GameMap.h"
 #include <stdio.h>
 #include <unistd.h>
+
+#include <qvbox.h>
 
 
 int main(int argc, char *argv[])
@@ -20,28 +23,6 @@ int main(int argc, char *argv[])
 
   // construct the application object
   MyGameApplication app(argc,argv);
-
-  // Show splash screen
-  app.showSplash();
-  app.initGame();
-  // To show splash for a while :-)
-  // Remove when initialization works
-  sleep(1);
-
-  // No error checking yet
-  // Maybe this should be done BEFORE app.initGame()?
-  GameMainWindow mw(0, 0, 800, 600);
-  mw.setCaption(versionString);
-  app.setMainWindow(&mw);
-
-  app.hideSplash();
-
-
-  // Ask's for user choose
-  // Must insert in GameApplication, only for testing/demonstration here.
-  GameModeSelectDialog dialog(&mw, 250,150,300,200, "Choose game mode");
-
-  printf("Result=%i\n", dialog.Show());
 
   return app.run();;
 }
