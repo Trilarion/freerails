a4bc9ced1c6a58796d3ed4619ca2b2eaa767c51a
bartcv <>
2001-08-30 19:44:06 +0000

    demo of GameElement and Player

--- src/main/game.cpp
+++ src/main/game.cpp
@@ -1,6 +1,19 @@
 #include "game.h"
+#include "iostream.h"
+
+#include "Player.h"
+#include "GameElement.h"
 
 MyGameApplication::MyGameApplication(int argc, char *argv[]):GameApplication(argc, argv) {
+  // Some rather silly demonstration code:
+  Player pl("me");
+  cerr << "Name: " << pl.getName() << endl;
+  cerr << "Adress: " << &pl << endl;
+  GameElement element(&pl, "foo");
+  element.setPlayer(&pl);
+  cerr << "Adress: " << element.getPlayer() << endl;
+  pl.addGameElement(&element);
+
 
 }
 
