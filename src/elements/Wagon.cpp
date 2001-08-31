d0291545aeca62c2996149c0257768f33a4b6bbd
rivol <>
2001-08-31 21:41:21 +0000

    Chaged some things

--- src/elements/Wagon.cpp
+++ src/elements/Wagon.cpp
@@ -4,19 +4,8 @@
 
 #include "Wagon.h"
 
-Wagon::Wagon(Train* tr, WagonType ty, Wagon* previous) :
-    train(tr),
+Wagon::Wagon(WagonType ty, Player* p) :
+    GameElement(p, ""),
     type(ty)
 {
-  if(previous == 0l)
-  {
-    // This wagon is next to train
-    next = train->firstWagon();
-  }
-  else
-  {
-    next = previous->getNext();
-    previous->setNext(this);
-  }
-  train->addWagon(this);
 }
--- src/elements/Wagon.h
+++ src/elements/Wagon.h
@@ -1,47 +1,39 @@
+/*
+ * $Id$
+ * Wagon class
+ */
 #ifndef __WAGON_H__
 #define __WAGON_H__
 
 #include "GameElement.h"
-/** Class that represents wagon
+#include "Player.h"
+
+/** @short Class that represents wagon
+  *
+  * @author Rivo Laks <rivolaks@hot.ee>
+  * @version $Id$
   */
-class Wagon: public GameElement
+class Wagon : public GameElement
 {
   public:
-    /** Constructs new wagon, which will be connected to train
-      * Wagon will be positioned after wagon previous
-      * If wagon previous is NULL, then this wagon will be immediatly after
-      * train
-      */
-    Wagon(Train* train, WagonType type, Wagon* previous = 0l);
-    ~Wagon();
-    /** Sets next wagon to next
-      * WARNING: This method may be useless, if we'll use vector to store wagons
-      * of train in train element
-      */
-    void setNext(Wagon* n) { next = n; };
-    /** Returns pointer to next wagon
-      * WARNING: This method may be useless, if we'll use vector to store wagons
-      * of train in train element
-      */
-    Wagon* getNext() { return next; };
-    /** If this is last wagon returns true, else false */
-    bool isLast() { if(next == 0l) return true; else return false; };
-    /** Returns pointer to train */
-    Train* getTrain() { return train; };
     /** Enumeration of wagon types
-	  * Currently has all wagon types from both America and Europe (from
-	  * http://freerails.sourceforge.net/elements.html)
-	  */
+  	  * Currently has all wagon types from both America and Europe (from
+	    * http://freerails.sourceforge.net/elements.html)
+	    */
     enum WagonType { Mail = 0, Passenger, Wine, Grape, Armament, Fertilizer,
         Textile, Steel, Nitrate, Wool, Coal, Caboose, Beer, Livestock, Goods,
         Hop, Chemicale, Cotton };
+    /** Constructs new wagon
+      * Wagon must be positioned by train that constructed it
+      */
+    Wagon(WagonType type, Player* p);
+    ~Wagon();
     /** Return type of wagon */
     WagonType getType() { return type; };
     /** Sets type of wagon */
     void setType(WagonType t) { type = t; };
+
   private:
-    Train* train;
-    Wagon* next;
     WagonType type;
 };
 
