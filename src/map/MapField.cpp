d8b7679616efb9e6cbcd95c39ba5b82ad3c6e8b2
opi <>
2001-09-21 17:00:42 +0000

    update MapField to needed ground parameters and functions

--- src/map/MapField.cpp
+++ src/map/MapField.cpp
@@ -4,7 +4,13 @@
 
 #include "MapField.h"
 
-MapField::MapField() {
+MapField::MapField(unsigned short _type, unsigned short _cost, unsigned short _height) {
+
+  type = _type;
+  cost = _cost;
+  height = _height;
+  element = NULL;
+//  Track = NULL;
 }
 
 MapField::~MapField() {
--- src/map/MapField.h
+++ src/map/MapField.h
@@ -6,19 +6,36 @@
 #define __MAPFIELD_H__
 
 #include "../elements/GameElement.h"
+//#include "../elements/Track.h"
 
 class MapField {
 public:
   /** Constructor */
-  MapField();
+  MapField(unsigned short _type, unsigned short _cost, unsigned short _height);
   /** Destructor */
   virtual ~MapField();
+  
+  void setType(unsigned short _type) { type=_type; };
+  unsigned short getType() { return type; };
+  
+  void setCost(unsigned short _cost) { cost=_cost; };
+  unsigned short getCost() { return cost; };
+  
+  void setHeight(unsigned short _height) { height=_height; };
+  unsigned short getHeight() { return height; };
+  
+  void setElement(GameElement* _element) { element=_element; };
+  GameElement* getElement() { return element; };
+  
+//  void setTrack(Track* _track) { track=_track; };
+//  Track* getTrack() { return track; };
 
 private:
   unsigned short type;
   unsigned short cost;
   unsigned short height;
   GameElement* element;
+//  Track* track;
 };
 
 #endif // __MAPFIELD_H__
