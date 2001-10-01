e16c27f3852b20a77e4154a1a40541f6d162bc40
opi <>
2001-10-01 11:34:02 +0000

    change path

--- src/wrapper/paragui/GamePanel.cpp
+++ src/wrapper/paragui/GamePanel.cpp
@@ -8,11 +8,11 @@ GamePanel::GamePanel(GameMainWindow* parent, int x, int y, int w, int h):
 PG_GradientWidget(parent->getWidget(), PG_Rect(x,y,w,h), "GradientWidget") {
   SetBackgroundBlend(0);
   trackButton=new PG_Button(this,1,PG_Rect(5,400,25,25));
-  trackButton->SetIcon("./data/graphics/ui/buttons/build_track_up.png",
-			"./data/graphics/ui/buttons/build_track_down.png");
+  trackButton->SetIcon("graphics/ui/buttons/build_track_up.png",
+			"graphics/ui/buttons/build_track_down.png");
   stationButton=new PG_Button(this,2,PG_Rect(35,400,25,25));
-  stationButton->SetIcon("./data/graphics/ui/buttons/build_station_up.png",
-			 "./data/graphics/ui/buttons/build_station_down.png");
+  stationButton->SetIcon("graphics/ui/buttons/build_station_up.png",
+			 "graphics/ui/buttons/build_station_down.png");
 }
 
 GamePanel::~GamePanel() {
