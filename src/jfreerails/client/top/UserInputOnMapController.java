c1cb56c4190113e66ce1c85b61511035da152ade
lindsal <>
2003-04-04 20:28:55 +0000

    Add javadoc package comments and simple train schedules

--- src/jfreerails/client/top/UserInputOnMapController.java
+++ src/jfreerails/client/top/UserInputOnMapController.java
@@ -14,17 +14,15 @@ import jfreerails.controller.pathfinder.TrainPathFinder;
 
 public class UserInputOnMapController implements CursorEventListener {
 
-	
 	private StationTypesPopup stationTypesPopup;
 
 	private MapViewJComponent mapView;
 
 	private TrackMoveProducer trackBuilder;
-	
-	private FreerailsCursor cursor;
-	
-	private DialogueBoxController dialogueBoxController;
 
+	private FreerailsCursor cursor;
+
+	private DialogueBoxController dialogueBoxController;
 
 	/**
 	 * @see jfreerails.client.event.CursorEventListener#cursorOneTileMove(jfreerails.client.event.CursorEvent)
@@ -34,12 +32,8 @@ public class UserInputOnMapController implements CursorEventListener {
 
 			trackBuilder.buildTrack(ce.oldPosition, ce.vector);
 			Point tile = new Point();
-			for (tile.x = ce.oldPosition.x - 1;
-				tile.x < ce.oldPosition.x + 2;
-				tile.x++) {
-				for (tile.y = ce.oldPosition.y - 1;
-					tile.y < ce.oldPosition.y + 2;
-					tile.y++) {
+			for (tile.x = ce.oldPosition.x - 1; tile.x < ce.oldPosition.x + 2; tile.x++) {
+				for (tile.y = ce.oldPosition.y - 1; tile.y < ce.oldPosition.y + 2; tile.y++) {
 					mapView.refreshTile(tile.x, tile.y);
 				}
 			}
@@ -48,25 +42,24 @@ public class UserInputOnMapController implements CursorEventListener {
 			System.out.println("No track builder available!");
 		}
 	}
-	
-	public void setup(
-	MapViewJComponent mv,
-			TrackMoveProducer trackBuilder,
-			
-			StationTypesPopup stPopup, FreerailsCursor fc,
-			DialogueBoxController dbc){
-			this.dialogueBoxController=dbc;
-			this.mapView = mv;			
-			this.stationTypesPopup = stPopup;
-			
-			this.trackBuilder = trackBuilder;
-		
 
-			this.cursor = fc;
-			
-			cursor.addCursorEventListener(this);
-			
-		}
+	public void setup(
+		MapViewJComponent mv,
+		TrackMoveProducer trackBuilder,
+		StationTypesPopup stPopup,
+		FreerailsCursor fc,
+		DialogueBoxController dbc) {
+		this.dialogueBoxController = dbc;
+		this.mapView = mv;
+		this.stationTypesPopup = stPopup;
+
+		this.trackBuilder = trackBuilder;
+
+		this.cursor = fc;
+
+		cursor.addCursorEventListener(this);
+
+	}
 
 	/**
 	 * @see jfreerails.client.event.CursorEventListener#cursorJumped(jfreerails.client.event.CursorEvent)
@@ -79,27 +72,45 @@ public class UserInputOnMapController implements CursorEventListener {
 	 * @see jfreerails.client.event.CursorEventListener#cursorKeyPressed(jfreerails.client.event.CursorEvent)
 	 */
 	public void cursorKeyPressed(CursorEvent ce) {
-		if (ce.keyEvent.getKeyCode() == KeyEvent.VK_F7) {
-			buildTrain(ce);
-		} else if (ce.keyEvent.getKeyCode() == KeyEvent.VK_F8) {
-			System.out.println("Build station");
-			float scale = mapView.getScale();
-			Point tile = new Point(ce.newPosition); //defensive copy.
-			Dimension tileSize = new Dimension((int) scale, (int) scale);
-			int x = tile.x * tileSize.width;
-			int y = tile.y * tileSize.height;
-			stationTypesPopup.show(mapView, x, y, tile);
-			//stationBuilder.buildStation(ce.newPosition);
-			//repaintMap(ce);
-		} else if (ce.keyEvent.getKeyCode() == KeyEvent.VK_T) {
 
-			TrainPathFinder.setTarget(ce.newPosition.x, ce.newPosition.y);
-			System.out.println(
-				"The target for the train pathfinder is now: "
-					+ ce.newPosition.x
-					+ ", "
-					+ ce.newPosition.y);
+		switch (ce.keyEvent.getKeyCode()) {
+			case KeyEvent.VK_F7 :
+				{
+					buildTrain(ce);
+					break;
+				}
+			case KeyEvent.VK_F8 :
+				{
+					System.out.println("Build station");
+					float scale = mapView.getScale();
+					Point tile = new Point(ce.newPosition); //defensive copy.
+					Dimension tileSize = new Dimension((int) scale, (int) scale);
+					int x = tile.x * tileSize.width;
+					int y = tile.y * tileSize.height;
+					stationTypesPopup.show(mapView, x, y, tile);
+					break;
 
+				}
+			case KeyEvent.VK_F1 :
+				{
+					TrainPathFinder.setTarget(0, ce.newPosition.x, ce.newPosition.y);
+					break;
+				}
+			case KeyEvent.VK_F2 :
+				{
+					TrainPathFinder.setTarget(1, ce.newPosition.x, ce.newPosition.y);
+					break;
+				}
+			case KeyEvent.VK_F3 :
+				{
+					TrainPathFinder.setTarget(2, ce.newPosition.x, ce.newPosition.y);
+					break;
+				}
+			case KeyEvent.VK_F4 :
+				{
+					TrainPathFinder.setTarget(3, ce.newPosition.x, ce.newPosition.y);
+					break;
+				}
 		}
 	}
 
@@ -108,6 +119,5 @@ public class UserInputOnMapController implements CursorEventListener {
 		dialogueBoxController.showSelectEngine();
 		//trainBuilder.buildTrain(ce.newPosition);
 	}
-
 
 }
