b77c878ff9c99171953550e2606c5ec564c70b64
lindsal <>
2005-01-15 05:37:10 +0000

    *** empty log message ***

---
+++ deploysite.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Deploys the website
+# This script is remotely called by the deploysite ant target.
+cd /home/groups/f/fr/freerails/
+rm htdocs/* -d -r -f
+gunzip website.tar.gz
+mv website.tar htdocs/
+cd htdocs/
+tar -xf website.tar
+rm website.tar
