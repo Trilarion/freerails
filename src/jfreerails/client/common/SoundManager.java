3eed92be1f09e1b0a0598f544022c864280ef6a8
lindsal <>
2005-01-24 23:31:42 +0000

    Second attempt at fixing bug 1103632 (Sound on Linux)

--- src/jfreerails/client/common/SoundManager.java
+++ src/jfreerails/client/common/SoundManager.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -14,134 +15,165 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-
 /**
- * This class is responsible for loading and playing sounds.  Samples are read into a byte arrays so that
- * they don't need to be loaded from disk each time they are played.  When a sample is played, the rate at which
- * it is played back is varied randomly so that it does not always sound the same.
- *
- *  @author Luke
- *
+ * This class is responsible for loading and playing sounds. Samples are read
+ * into a byte arrays so that they don't need to be loaded from disk each time
+ * they are played. When a sample is played, the rate at which it is played back
+ * is varied randomly so that it does not always sound the same.
+ * 
+ * @author Luke
+ * 
  */
-public class SoundManager implements ModelRootListener {
-    private static final Logger logger = Logger.getLogger(SoundManager.class.getName());
-    Random r = new Random();
-    private boolean playSounds = true;
-    private static final SoundManager instance = new SoundManager();
+public class SoundManager implements ModelRootListener, LineListener {
 
-    /**
-     *  Stores the audio data and properties of a sample.      
-     *
-     */
-    private static class Sample {
-        DataLine.Info info;
-        AudioFormat format;
-        int size;
-        byte[] audio;
-    }
+	/**
+	 * Stores the audio data and properties of a sample.
+	 * 
+	 */
+	private static class Sample {
 
-    private HashMap<String, Sample> samples = new HashMap<String, Sample>();
+		byte[] audio;
 
-    private SoundManager() {
-    }
+		AudioFormat format;
 
-    public void addClip(String s)
-        throws IOException, UnsupportedAudioFileException, 
-            LineUnavailableException {
-    	if (samples.containsKey(s)) {
-            return;
-        }
-    	
-        URL url = getClass().getResource(s);
-        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(loadStream(
-                    url.openStream()));
+		DataLine.Info info;
 
-        Sample sample = new Sample();
+		int size;
+	}
 
-        sample.format = audioInputStream.getFormat();
-        sample.size = (int)(sample.format.getFrameSize() * audioInputStream.getFrameLength());
-        sample.audio = new byte[sample.size];
-        sample.info = new DataLine.Info(Clip.class, sample.format, sample.size);
-        audioInputStream.read(sample.audio, 0, sample.size);
-        samples.put(s, sample);
-    }
+	private static final Logger logger = Logger.getLogger(SoundManager.class
+			.getName());
 
-    private ByteArrayInputStream loadStream(InputStream inputstream)
-        throws IOException {
-        ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
-        byte[] data = new byte[1024];
+	private static final SoundManager soundManager = new SoundManager();
 
-        for (int i = inputstream.read(data); i != -1;
-                i = inputstream.read(data)) {
-            bytearrayoutputstream.write(data, 0, i);
-        }
+	public static SoundManager getSoundManager() {
+		return soundManager;
+	}
 
-        inputstream.close();
-        bytearrayoutputstream.close();
-        data = bytearrayoutputstream.toByteArray();
+	public static void main(String[] args) {
 
-        return new ByteArrayInputStream(data);
-    }
+		SoundManager soundPlayer = getSoundManager();
+		for (int i = 0; i < 100; i++) {
+			soundPlayer.playSound("/jfreerails/client/sounds/cash.wav", 10);
 
-    public void playSound(String s, int loops) {
-        if (playSounds) {
-            try {
-                if (!samples.containsKey(s)) {
-                    addClip(s);
-                }
-
-                Sample sample = samples.get(s);
-                
-// The code below is commented out as a fix for bug 1103632	(Sound on Linux).
-//                //Add a random component to the sample rate so that the samples 
-//                //don't always sound the same.
-//                float frameRate = (1 + (r.nextFloat() - 0.5f) / 3) * sample.format.getFrameRate();
-//
-//                //Make sure the frameRate is a sensible value, i.e.  between 4 kHz and 48 kHz.
-//                frameRate = Math.min(frameRate, 48000f);
-//                frameRate = Math.max(frameRate, 4000f);
-//
-//                AudioFormat format = new AudioFormat(sample.format.getEncoding(),
-//                        sample.format.getSampleRate(),
-//                        sample.format.getSampleSizeInBits(),
-//                        sample.format.getChannels(),
-//                        sample.format.getFrameSize(), frameRate,
-//                        sample.format.isBigEndian());
-                Clip clip = (Clip)AudioSystem.getLine(sample.info);
-                clip.open(sample.format, sample.audio, 0, sample.size);
-                clip.loop(loops);
-            } catch (LineUnavailableException e) {
-                logger.warning(e.getMessage());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        SoundManager soundPlayer = getSoundManager();
-        soundPlayer.playSound("/jfreerails/client/sounds/cash.wav", 10);
-
-        try {
-            Thread.sleep(40);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        soundPlayer.playSound("/jfreerails/client/sounds/cash.wav", 10);
-    }
-
-    public static SoundManager getSoundManager() {
-        return instance;
-    }  
-
-	public void propertyChange(ModelRoot.Property p, Object before, Object after) {
-		if(p.equals(ModelRoot.Property.PLAY_SOUNDS)){
-			Boolean b = (Boolean)after;
-            playSounds = b.booleanValue();
+			try {
+				Thread.sleep(40);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
+
+	private int maxLines;
+
+	private Mixer mixer;
+
+	private boolean playSounds = true;
+
+	Random r = new Random();
+
+	private HashMap<String, Sample> samples = new HashMap<String, Sample>();
+
+	private final LinkedList<Clip> voices = new LinkedList<Clip>();
+
+	private SoundManager() {
+		AudioFormat format2 = new AudioFormat(8000f, 16, 1, true, false);
+		DataLine.Info info2 = new DataLine.Info(null, format2, 0);
+		for (Mixer.Info mo : AudioSystem.getMixerInfo()) {
+
+			mixer = AudioSystem.getMixer(mo);
+			maxLines = mixer.getMaxLines(info2);
+			if (maxLines >= 32)
+				break; // Java Sound Audio Engine, version 1.0 satisfies this.
+		}
+		logger.fine("Sound Mixer: " + mixer.getMixerInfo() + "(" + maxLines
+				+ " voices).");
+
+	}
+
+	public void addClip(String s) throws IOException,
+			UnsupportedAudioFileException, LineUnavailableException {
+		if (samples.containsKey(s)) {
+			return;
+		}
+
+		URL url = getClass().getResource(s);
+		AudioInputStream audioInputStream = AudioSystem
+				.getAudioInputStream(loadStream(url.openStream()));
+
+		Sample sample = new Sample();
+
+		sample.format = audioInputStream.getFormat();
+		sample.size = (int) (sample.format.getFrameSize() * audioInputStream
+				.getFrameLength());
+		sample.audio = new byte[sample.size];
+		sample.info = new DataLine.Info(Clip.class, sample.format, sample.size);
+		audioInputStream.read(sample.audio, 0, sample.size);
+		samples.put(s, sample);
+	}
+
+	private ByteArrayInputStream loadStream(InputStream inputstream)
+			throws IOException {
+		ByteArrayOutputStream bytearrayoutputstream = new ByteArrayOutputStream();
+		byte[] data = new byte[1024];
+
+		for (int i = inputstream.read(data); i != -1; i = inputstream
+				.read(data)) {
+			bytearrayoutputstream.write(data, 0, i);
+		}
+
+		inputstream.close();
+		bytearrayoutputstream.close();
+		data = bytearrayoutputstream.toByteArray();
+
+		return new ByteArrayInputStream(data);
+	}
+
+	public void playSound(String s, int loops) {
+		if (playSounds) {
+			try {
+				if (!samples.containsKey(s)) {
+					addClip(s);
+				}
+
+				Sample sample = samples.get(s);
+
+				Clip clip;
+				if (voices.size() < maxLines) {
+					clip = (Clip) mixer.getLine(sample.info);
+				} else {
+					clip = voices.removeFirst();
+					clip.stop();
+					clip.flush();
+					clip.close();
+				}
+				clip.addLineListener(this);
+				clip.open(sample.format, sample.audio, 0, sample.size);
+				clip.loop(loops);
+				voices.add(clip);
+			} catch (LineUnavailableException e) {
+				logger.warning(e.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void propertyChange(ModelRoot.Property p, Object before, Object after) {
+		if (p.equals(ModelRoot.Property.PLAY_SOUNDS)) {
+			Boolean b = (Boolean) after;
+			playSounds = b.booleanValue();
+		}
+	}
+
+	public void update(LineEvent event) {
+		// TODO free up resources when we have finished playing a clip.
+	}
+
 }
