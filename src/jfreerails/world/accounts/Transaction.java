d408da4cbcc1c1c32bd128e503efa3f16846f460
lindsal <>
2005-02-05 00:35:43 +0000

    Add the Transaction category 'Stock'

--- src/jfreerails/world/accounts/Transaction.java
+++ src/jfreerails/world/accounts/Transaction.java
@@ -16,7 +16,7 @@ import jfreerails.world.common.Money;
  */
 public interface Transaction extends FreerailsSerializable {
     
-	public enum Category{TRACK, CARGO_DELIVERY, TRACK_MAINTENANCE, TRAIN_MAINTENANCE, STATION_MAINTENANCE, TRAIN, MISC_INCOME, INTEREST_CHARGE, BOND, ISSUE_STOCK, INDUSTRIES, STATIONS, BRIDGES};	  
+	public enum Category{TRACK, CARGO_DELIVERY, TRACK_MAINTENANCE, TRAIN_MAINTENANCE, STATION_MAINTENANCE, TRAIN, MISC_INCOME, INTEREST_CHARGE, BOND, ISSUE_STOCK, INDUSTRIES, STATIONS, BRIDGES, STOCK};	  
 
     /** Positive means credit. */
     Money getValue();
