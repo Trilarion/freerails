9b4a00565f4f19662e8054b12f884ff4a16c011c
cymric_npg <cymric_npg@97cc0ca2-483e-0410-8e64-ffdcb17a36b8>
2007-11-06 22:04:46 +0000

    git-svn-id: https://svn.code.sf.net/p/freerails2/code@3 97cc0ca2-483e-0410-8e64-ffdcb17a36b8

--- deploysite.sh
+++ deploysite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Deploys the website
 # This script is remotely called by the deploysite ant target.
-cd /home/groups/f/fr/freerails/
+cd /home/groups/f/fr/freerails2/
 rm htdocs/* -d -r -f
 gunzip website.tar.gz
 mv website.tar htdocs/
