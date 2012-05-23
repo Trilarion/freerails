4ead3b23068394407b2c72b9cf3cae44bf3e40e6
lukeyj <lukeyj@4abc17fb-09e1-40b8-9464-7a7a35ee2e34>
2012-05-23 22:29:56 +0000

    Fixed load bug resources
    git-svn-id: https://svn.code.sf.net/p/railz2/code/trunk@11 4abc17fb-09e1-40b8-9464-7a7a35ee2e34

--- src/org/railz/client/view/MainMapAndOverviewMapMediator.java
+++ src/org/railz/client/view/MainMapAndOverviewMapMediator.java
@@ -22,74 +22,75 @@
  */
 
 package org.railz.client.view;
-import java.awt.Cursor;
+
 import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.event.MouseEvent;
 
 import javax.swing.JComponent;
 import javax.swing.JViewport;
 import javax.swing.event.ChangeEvent;
 
-/** This class mediates between the main map view and the overview
- * map view.  It does the following:<br>
- * (1) Updates the rectangle on the overview map when the visible
- * rectangle of the main map changes.<br>
- * (2) Updates the main map visible rectangle when the user clicks
+/**
+ * This class mediates between the main map view and the overview map view. It
+ * does the following:<br>
+ * (1) Updates the rectangle on the overview map when the visible rectangle of
+ * the main map changes.<br>
+ * (2) Updates the main map visible rectangle when the user clicks on the
+ * overview map.<br>
+ * (3) Updates the main map visible rectangle when the user drags the rectangle
  * on the overview map.<br>
- * (3) Updates the main map visible rectangle when the user drags
- * the rectangle on the overview map.<br>
- * (4) Changes the mouse cursor to indicate that the rectangle on
- * the overview map is draggable when the mouse moves into the
- * rectangle.
- *
+ * (4) Changes the mouse cursor to indicate that the rectangle on the overview
+ * map is draggable when the mouse moves into the rectangle.
+ * 
  * @author Luke Lindsay
  * @version 1.0
  */
-
 public class MainMapAndOverviewMapMediator {
-	private  JComponent overviewMapJPanel;
-	private  JViewport viewport;
-	private  JComponent mainMap;
-
-	/**
-	 * Position of the overview map rectangle relative to the overview map
-	 * origin
-	 */
-	private  Rectangle currentVisRect;
-
-	/**
-	 * The previous position of the overview map rectangle
-	 */
-	private Point oldLocation;
-
-	public MainMapAndOverviewMapMediator(){
-	    currentVisRect = new Rectangle(10, 10, 10, 10);
-	}
-
-	public void setOverviewMap(JComponent omv) {
-	    overviewMapJPanel = omv;
-	}
-
-	/**
-	 * @param v the main map viewport
-	 * @param mm The Main map component
-	 */
-	public void setMainMap (JViewport v, JComponent mm) {
-	    mainMap = mm;
-	    viewport = v;
-
-	    viewport.addChangeListener
-		(new javax.swing.event.ChangeListener() {
-		    public void stateChanged(ChangeEvent e) {
-		    updateObservedRect();
-		    }
-		    });
-	}
-
+    private JComponent overviewMapJPanel;
+    private JViewport viewport;
+    private JComponent mainMap;
+    
+    /**
+     * Position of the overview map rectangle relative to the overview map
+     * origin
+     */
+    private Rectangle currentVisRect;
+    
+    /**
+     * The previous position of the overview map rectangle
+     */
+    private Point oldLocation;
+    
+    public MainMapAndOverviewMapMediator() {
+	currentVisRect = new Rectangle(10, 10, 10, 10);
+    }
+    
+    public void setOverviewMap(JComponent omv) {
+	overviewMapJPanel = omv;
+    }
+    
+    /**
+     * @param v
+     *            the main map viewport
+     * @param mm
+     *            The Main map component
+     */
+    public void setMainMap(JViewport v, JComponent mm) {
+	mainMap = mm;
+	viewport = v;
+	
+	viewport.addChangeListener(new javax.swing.event.ChangeListener() {
+	    public void stateChanged(ChangeEvent e) {
+		updateObservedRect();
+	    }
+	});
+    }
+    
     /**
      * Scroll the main map to the position indicated by the coordinates.
-     * @param overviewMapPos position indicated by the overview map coords.
+     * 
+     * @param overviewMapPos
+     *            position indicated by the overview map coords.
      */
     public void setMainMapPosition(Point overviewMapPos) {
 	if (oldLocation == null) {
@@ -98,27 +99,27 @@ public class MainMapAndOverviewMapMediator {
 	    oldLocation.x = currentVisRect.x;
 	    oldLocation.y = currentVisRect.y;
 	}
-
+	
 	if (mainMap == null)
 	    return;
-
+	
 	int deltaX = overviewMapPos.x - oldLocation.x;
 	int deltaY = overviewMapPos.y - oldLocation.y;
 	Rectangle r = mainMap.getVisibleRect();
-
+	
 	int overviewScale = overviewMapJPanel.getPreferredSize().width;
 	int mainMapScale = mainMap.getWidth();
-
+	
 	/* calculate the delta to scroll */
 	int scaledDeltaX = (int) (deltaX * mainMapScale / overviewScale);
 	int scaledDeltaY = (int) (deltaY * mainMapScale / overviewScale);
-
+	
 	r.x += scaledDeltaX;
 	r.y += scaledDeltaY;
-
+	
 	mainMap.scrollRectToVisible(r);
     }
-
+    
     /**
      * Update the overview map with the coordinates from the main map, and
      * redraw the overview map.
@@ -126,12 +127,12 @@ public class MainMapAndOverviewMapMediator {
     public void updateObservedRect() {
 	if (mainMap == null || overviewMapJPanel == null)
 	    return;
-
+	
 	Rectangle r = mainMap.getVisibleRect();
 	int overviewScale = overviewMapJPanel.getPreferredSize().width;
 	int mainMapScale = mainMap.getWidth();
 	if (0 != (overviewScale * mainMapScale)) {
-	    //avoid division by zero.
+	    // avoid division by zero.
 	    currentVisRect.x = (int) (r.x * overviewScale / mainMapScale);
 	    currentVisRect.y = (int) (r.y * overviewScale / mainMapScale);
 	    currentVisRect.width = (int) (r.width * overviewScale / mainMapScale);
@@ -139,12 +140,12 @@ public class MainMapAndOverviewMapMediator {
 	    overviewMapJPanel.repaint();
 	}
     }
-	
-	/**
-	 * @return the rectangle describing the area of the overview map
-	 * visible on the main map.
-	 */
-	public Rectangle getMapVisibleRectangle() {
-	    return currentVisRect;
-	}
+    
+    /**
+     * @return the rectangle describing the area of the overview map visible on
+     *         the main map.
+     */
+    public Rectangle getMapVisibleRectangle() {
+	return currentVisRect;
+    }
 }
--- src/org/railz/util/Resources.java
+++ src/org/railz/util/Resources.java
@@ -15,57 +15,65 @@
  */
 
 package org.railz.util;
- 
+
 import java.io.Serializable;
-import java.util.*;
-import java.text.*;
+import java.text.MessageFormat;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 /**
  * Utility class for obtaining localised resource strings.
  */
 public final class Resources {
     public static class ResourceKey implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3498239306084923539L;
 	private String string;
 	private Serializable[] params;
-
+	
 	private ResourceKey(String string, Serializable[] params) {
 	    this.string = string;
 	    this.params = params;
 	}
     }
-
+    
     /**
-     * @param string The localisation key.
-     * @param params The parameters to be passed into MessageFormat.format()
+     * @param string
+     *            The localisation key.
+     * @param params
+     *            The parameters to be passed into MessageFormat.format()
      */
-    public static ResourceKey getResourceKey(String string, Serializable[]
-	    params) {
+    public static ResourceKey getResourceKey(String string, Serializable[] params) {
 	return new ResourceKey(string, params);
     }
     
     public static ResourceKey getResourceKey(String string) {
 	return new ResourceKey(string, new Serializable[0]);
     }
-
+    
     private static ResourceBundle bundle;
     private static ResourceBundle externalResource;
-   
-   static {
-       try {
-	   bundle = ResourceBundle.getBundle
-	       ("org.railz.data.l10n.jfreerails");
-       } catch (MissingResourceException e) {
-	   bundle = null;
-       }
-   }
-
+    
+    static {
+	try {
+	    bundle = ResourceBundle.getBundle("org.railz.data.l10n.jfreerails");
+	} catch (MissingResourceException e) {
+	    bundle = null;
+	}
+    }
+    
     /**
      * Provided for especially long localised texts.
+     * 
      * @return a localised string corresponding to the specified key and object
-     * @param defaultString The string to be localised. This will be returned
-     * as the default if no localised string is found
-     * @param key The key to be searched for in the resource bundles. This can
-     * be much shorter than the default string
+     * @param defaultString
+     *            The string to be localised. This will be returned as the
+     *            default if no localised string is found
+     * @param key
+     *            The key to be searched for in the resource bundles. This can
+     *            be much shorter than the default string
      */
     public static String get(String key, String defaultString) {
 	try {
@@ -84,23 +92,24 @@ public final class Resources {
 	}
 	return defaultString;
     }
-
+    
     /**
      * @return a localised string corresponding to the specified key and object
-     * @param defaultString The string to be localised. This will be returned
-     * as the default if no localised string is found
+     * @param defaultString
+     *            The string to be localised. This will be returned as the
+     *            default if no localised string is found
      */
     public static String get(String defaultString) {
 	return get(defaultString, defaultString);
     }
-
+    
     public static String get(ResourceKey key) {
 	return MessageFormat.format(key.string, key.params);
     }
-
+    
     /**
-     * Set an external resource bundle that is used as a back-up if the
-     * default does not have a given resource
+     * Set an external resource bundle that is used as a back-up if the default
+     * does not have a given resource
      */
     public static void setExternalResourceBundle(ResourceBundle rb) {
 	externalResource = rb;
