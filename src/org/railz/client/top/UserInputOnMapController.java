e7367202615235afc6b978a9851d891c74a39ae4
lukeyj <lukeyj@4abc17fb-09e1-40b8-9464-7a7a35ee2e34>
2013-12-28 16:34:52 +0000

    Handle unexpected exceptions from UI without crashing.
    git-svn-id: https://svn.code.sf.net/p/railz2/code/trunk@20 4abc17fb-09e1-40b8-9464-7a7a35ee2e34

--- src/org/railz/client/top/UserInputOnMapController.java
+++ src/org/railz/client/top/UserInputOnMapController.java
@@ -19,117 +19,152 @@ package org.railz.client.top;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.railz.client.model.CursorEvent;
 import org.railz.client.model.CursorEventListener;
 import org.railz.client.model.MapCursor;
 import org.railz.client.model.ModelRoot;
-import org.railz.client.view.*;
+import org.railz.client.view.DialogueBoxController;
+import org.railz.client.view.GUIRoot;
+import org.railz.client.view.MapViewJComponent;
+import org.railz.client.view.StationTypesPopup;
+import org.railz.config.LogManager;
 import org.railz.controller.TrackMoveProducer;
 import org.railz.controller.UncommittedMoveReceiver;
 import org.railz.move.MoveStatus;
 
-
 public class UserInputOnMapController implements CursorEventListener {
-    private StationTypesPopup stationTypesPopup;
-    private MapViewJComponent mapView;
-    private TrackMoveProducer trackBuilder;
-    private MapCursor cursor;
-    private DialogueBoxController dialogueBoxController;
-    private UncommittedMoveReceiver trackMoveExecutor;
-    private ModelRoot modelRoot;
+	private StationTypesPopup stationTypesPopup;
+	private MapViewJComponent mapView;
+	private TrackMoveProducer trackBuilder;
+	private MapCursor cursor;
+	private DialogueBoxController dialogueBoxController;
+	private UncommittedMoveReceiver trackMoveExecutor;
+	private final ModelRoot modelRoot;
 
-    public UserInputOnMapController(ModelRoot mr) {
-        modelRoot = mr;
-    }
+	private static final String CLASS_NAME = UserInputOnMapController.class
+			.getName();
+	private static final Logger LOGGER = LogManager.getLogger(CLASS_NAME);
 
-    public void cursorOneTileMove(CursorEvent ce) {
-        if (null != trackBuilder) {
-            MoveStatus ms = trackBuilder.buildTrack(ce.oldPosition, ce.vector);
+	public UserInputOnMapController(ModelRoot mr) {
+		modelRoot = mr;
+	}
 
-            if (ms.ok) {
-                cursor.setMessage("");
-            } else {
-                cursor.setMessage(ms.message);
-            }
+	public void setup(GUIRoot gr, MapViewJComponent mv, StationTypesPopup stp) {
+		trackMoveExecutor = modelRoot.getReceiver();
+		this.dialogueBoxController = gr.getDialogueBoxController();
+		this.mapView = mv;
 
-            Point tile = new Point();
+		this.trackBuilder = modelRoot.getTrackMoveProducer();
 
-            for (tile.x = ce.oldPosition.x - 1; tile.x < ce.oldPosition.x + 2;
-                    tile.x++) {
-                for (tile.y = ce.oldPosition.y - 1;
-                        tile.y < ce.oldPosition.y + 2; tile.y++) {
-                    mapView.refreshTile(tile.x, tile.y);
-                }
-            }
-        } else {
-            System.err.println("No track builder available!");
-        }
-    }
+		this.cursor = modelRoot.getCursor();
 
-    public void setup(GUIRoot gr,
-	    MapViewJComponent mv, StationTypesPopup stp) {
-        trackMoveExecutor = modelRoot.getReceiver();
-        this.dialogueBoxController = gr.getDialogueBoxController();
-        this.mapView = mv;
+		cursor.addCursorEventListener(this);
+		stationTypesPopup = stp;
+	}
 
-        this.trackBuilder = modelRoot.getTrackMoveProducer();
+	@Override
+	public void cursorOneTileMove(CursorEvent ce) {
+		final String methodName = "cursorOneTileMove";
+		try {
+			if (null != trackBuilder) {
+				MoveStatus ms = trackBuilder.buildTrack(ce.oldPosition,
+						ce.vector);
 
-        this.cursor = modelRoot.getCursor();
+				if (ms.ok) {
+					cursor.setMessage("");
+				} else {
+					cursor.setMessage(ms.message);
+				}
 
-        cursor.addCursorEventListener(this);
-	stationTypesPopup = stp;
-    }
+				Point tile = new Point();
 
-    public void cursorJumped(CursorEvent ce) {
-        if (trackBuilder.getTrackBuilderMode() == TrackMoveProducer.UPGRADE_TRACK) {
-            MoveStatus ms = trackBuilder.upgradeTrack(ce.newPosition);
+				for (tile.x = ce.oldPosition.x - 1; tile.x < ce.oldPosition.x + 2; tile.x++) {
+					for (tile.y = ce.oldPosition.y - 1; tile.y < ce.oldPosition.y + 2; tile.y++) {
+						mapView.refreshTile(tile.x, tile.y);
+					}
+				}
 
-            if (ms.ok) {
-                cursor.setMessage("");
-            } else {
-                cursor.setMessage(ms.message);
-            }
-        }
-    }
+			} else {
+				System.err.println("No track builder available!");
+			}
+		} catch (Exception ex) {
+			LOGGER.logp(Level.SEVERE, CLASS_NAME, methodName,
+					"Uncaught exception in UI. Investigate and resolve.", ex);
+		}
 
-    public void cursorKeyPressed(CursorEvent ce) {
-        switch (ce.keyEvent.getKeyCode()) {
-        case KeyEvent.VK_F8: {
-            //defensive copy.
-            Point tile = new Point(ce.newPosition);
+	}
 
-            //Check whether we can built a station here before proceeding.
-            if (stationTypesPopup.canBuiltStationHere(tile)) {
-                float scale = mapView.getScale();
-                Dimension tileSize = new Dimension((int)scale, (int)scale);
-                int x = tile.x * tileSize.width;
-                int y = tile.y * tileSize.height;
-                stationTypesPopup.showMenu(mapView, x, y, tile);
-            } else {
-                modelRoot.getUserMessageLogger().println("Can't" +
-                    " build station here!");
-            }
+	@Override
+	public void cursorJumped(CursorEvent ce) {
+		final String methodName = "cursorJumped";
 
-            break;
-        }
+		try {
+			if (trackBuilder.getTrackBuilderMode() == TrackMoveProducer.UPGRADE_TRACK) {
+				MoveStatus ms = trackBuilder.upgradeTrack(ce.newPosition);
 
-        case KeyEvent.VK_BACK_SPACE:
-            trackMoveExecutor.undoLastMove();
+				if (ms.ok) {
+					cursor.setMessage("");
+				} else {
+					cursor.setMessage(ms.message);
+				}
+			}
 
-            break;
+		} catch (Exception ex) {
+			LOGGER.logp(Level.SEVERE, CLASS_NAME, methodName,
+					"Uncaught exception in UI. Investigate and resolve.", ex);
+		}
+	}
 
-        case KeyEvent.VK_I: {
-            dialogueBoxController.showStationOrTerrainInfo(ce.newPosition.x,
-                ce.newPosition.y);
+	@Override
+	public void cursorKeyPressed(CursorEvent ce) {
+		final String methodName = "cursorKeyPressed";
+		try {
+			switch (ce.keyEvent.getKeyCode()) {
+			case KeyEvent.VK_F8: {
+				// defensive copy.
+				Point tile = new Point(ce.newPosition);
 
-            break;
-        }
+				// Check whether we can built a station here before proceeding.
+				if (stationTypesPopup.canBuiltStationHere(tile)) {
+					float scale = mapView.getScale();
+					Dimension tileSize = new Dimension((int) scale, (int) scale);
+					int x = tile.x * tileSize.width;
+					int y = tile.y * tileSize.height;
+					stationTypesPopup.showMenu(mapView, x, y, tile);
+				} else {
+					modelRoot.getUserMessageLogger().println(
+							"Can't" + " build station here!");
+				}
 
-        case KeyEvent.VK_C: {
-            mapView.centerOnTile(new Point(ce.newPosition.x, ce.newPosition.y));
+				break;
+			}
 
-            break;
-        }
-        }
-    }
+			case KeyEvent.VK_BACK_SPACE:
+				trackMoveExecutor.undoLastMove();
+
+				break;
+
+			case KeyEvent.VK_I: {
+				dialogueBoxController.showStationOrTerrainInfo(
+						ce.newPosition.x, ce.newPosition.y);
+
+				break;
+			}
+
+			case KeyEvent.VK_C: {
+				mapView.centerOnTile(new Point(ce.newPosition.x,
+						ce.newPosition.y));
+
+				break;
+			}
+			}
+		} catch (Exception ex) {
+			LOGGER.logp(Level.SEVERE, CLASS_NAME, methodName,
+					"Uncaught exception in UI. Investigate and resolve.", ex);
+		}
+
+	}
 }
