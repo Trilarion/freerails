Trilarion/freerails

commit 8448762da2694f18d3c9130ed9d8ccd4145fc244

refactoring

src/main/java/freerails/client/view/StationInfoPanel.java
@@ -315,16 +315,16 @@ public class StationInfoPanel extends JPanel implements View, WorldListListener 
         }
     }
 
-    public void listUpdated(PlayerKey playerKey, int index, FreerailsPrincipal principal) {
-        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(playerKey, index, false);
+    public void listUpdated(PlayerKey key, int index, FreerailsPrincipal principal) {
+        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(key, index, false);
     }
 
-    public void itemAdded(PlayerKey playerKey, int index, FreerailsPrincipal principal) {
-        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(playerKey, index, true);
+    public void itemAdded(PlayerKey key, int index, FreerailsPrincipal principal) {
+        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(key, index, true);
     }
 
-    public void itemRemoved(PlayerKey playerKey, int index, FreerailsPrincipal principal) {
-        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(playerKey, index, false);
+    public void itemRemoved(PlayerKey key, int index, FreerailsPrincipal principal) {
+        if (modelRoot.getPrincipal().equals(principal)) reactToUpdate(key, index, false);
     }
 
     void removeCloseButton() {

src/main/java/freerails/util/Vector2D.java
@@ -74,7 +74,7 @@ public final class Vector2D implements Serializable, Comparable<Vector2D> {
 
     @Override
     public String toString() {
-        return "Vector2D{" + x + ", " + y + '}';
+        return "(" + x + ", " + y + ")";
     }
 
     public static Vector2D add(Vector2D a, Vector2D b) {

commit 329741b4ff3d844b64cfcffbd5c8e63b654b3ea0

refactoring and fixed a bug introduced in 8448762da2694f18d3c9130ed9d8ccd4145fc244 MoveChainFork.java would not process CompositeMove recursively anymore (visible for example by a missing station icon after creating a station), went back to past state of code

src/main/java/freerails/client/ClientConfig.java
@@ -208,7 +208,7 @@ public final class ClientConfig {
      */
     public static final int SERVERUPDATE = 50;
 
-    public static final Vector2D tileSize = new Vector2D(WorldConstants.TILE_SIZE, WorldConstants.TILE_SIZE);
+    public static final Vector2D TILE_SIZE = new Vector2D(WorldConstants.TILE_SIZE, WorldConstants.TILE_SIZE);
     /**
      * Affects scroll direction and scroll speed relative to the cursor.
      * Examples:

src/main/java/freerails/client/renderer/TrainImages.java
@@ -46,9 +46,8 @@ public class TrainImages {
         sideOnFileName = "trains" + File.separator + "sideon" + File.separator + name + ".png";
         sideOnImage = imageManager.getImage(sideOnFileName);
 
+        TileTransition[] vectors = TileTransition.getTransitions();
         for (int direction = 0; direction < 8; direction++) {
-            TileTransition[] vectors = TileTransition.getTransitions();
-
             String overheadOnFileName = "trains" + File.separator + "overhead" + File.separator + name + '_' + vectors[direction].toAbrvString() + ".png";
             overheadImages[direction] = imageManager.getImage(overheadOnFileName);
         }

src/main/java/freerails/client/renderer/map/StationNamesRenderer.java
@@ -18,6 +18,7 @@
 
 package freerails.client.renderer.map;
 
+import freerails.client.ClientConfig;
 import freerails.client.ModelRootProperty;
 import freerails.model.world.PlayerKey;
 import freerails.util.ui.Painter;
@@ -30,7 +31,6 @@ import freerails.model.terrain.FullTerrainTile;
 import freerails.model.world.ReadOnlyWorld;
 
 import java.awt.*;
-import java.awt.font.FontRenderContext;
 import java.awt.font.TextLayout;
 import java.awt.geom.RoundRectangle2D;
 
@@ -44,10 +44,10 @@ public class StationNamesRenderer implements Painter {
     private static final Stroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f);
     private final ReadOnlyWorld world;
     private final ModelRoot modelRoot;
-    private final int fontSize;
-    private final Color bgColor;
-    private final Color textColor;
-    private final Font font;
+    private final int fontSize = 10;
+    private final Color bgColor = Color.BLACK;
+    private final Color textColor = Color.WHITE;
+    private final Font font = new Font("Arial", Font.PLAIN, fontSize);
 
     /**
      * @param world
@@ -56,10 +56,6 @@ public class StationNamesRenderer implements Painter {
     public StationNamesRenderer(ReadOnlyWorld world, ModelRoot modelRoot) {
         this.world = world;
         this.modelRoot = modelRoot;
-        fontSize = 10;
-        bgColor = Color.BLACK;
-        textColor = Color.WHITE;
-        font = new Font("Arial", Font.PLAIN, fontSize);
     }
 
     /**
@@ -67,49 +63,34 @@ public class StationNamesRenderer implements Painter {
      * @param newVisibleRectangle
      */
     public void paint(Graphics2D g, Rectangle newVisibleRectangle) {
-        int rectWidth;
-        int rectHeight;
-        int rectX;
-        int rectY;
-        float visibleAdvance;
-        float textX;
-        float textY;
-
-        Station tempStation;
-        String stationName;
-        int positionX;
-        int positionY;
 
         Boolean showStationNames = (Boolean) modelRoot.getProperty(ModelRootProperty.SHOW_STATION_NAMES);
         Boolean showStationBorders = (Boolean) modelRoot.getProperty(ModelRootProperty.SHOW_STATION_BORDERS);
 
-        FontRenderContext frc = g.getFontRenderContext();
-        TextLayout layout;
-
+        // for all players
         for (int i = 0; i < world.getNumberOfPlayers(); i++) {
             FreerailsPrincipal principal = world.getPlayer(i).getPrincipal();
 
             // draw station names onto map
-            WorldIterator wi = new NonNullElementWorldIterator(PlayerKey.Stations, world, principal);
 
-            while (wi.next()) { // loop over non null stations
-                tempStation = (Station) wi.getElement();
+            // for all stations of this player
+            WorldIterator worldIterator = new NonNullElementWorldIterator(PlayerKey.Stations, world, principal);
+            while (worldIterator.next()) { // loop over non null stations
+                Station station = (Station) worldIterator.getElement();
 
-                int x = tempStation.getStationP().x;
-                int y = tempStation.getStationP().y;
-                int xdisp = x * WorldConstants.TILE_SIZE;
-                int ydisp = y * WorldConstants.TILE_SIZE;
-                Rectangle stationBox = new Rectangle(xdisp - WorldConstants.TILE_SIZE * 3, ydisp - WorldConstants.TILE_SIZE * 3, WorldConstants.TILE_SIZE * 7, WorldConstants.TILE_SIZE * 7);
+                Vector2D location = station.getLocation();
+                Vector2D displayLocation = Vector2D.multiply(location, WorldConstants.TILE_SIZE);
+                Rectangle stationBox = new Rectangle(displayLocation.x - WorldConstants.TILE_SIZE * 3, displayLocation.y - WorldConstants.TILE_SIZE * 3, WorldConstants.TILE_SIZE * 7, WorldConstants.TILE_SIZE * 7);
                 if (newVisibleRectangle != null && !newVisibleRectangle.intersects(stationBox)) {
                     continue; // station box not visible
                 }
                 // First draw station sphere of influence
                 if (showStationBorders) {
-                    FullTerrainTile tile = (FullTerrainTile) world.getTile(new Vector2D(x, y));
+                    FullTerrainTile tile = (FullTerrainTile) world.getTile(location);
                     int radius = tile.getTrackPiece().getTrackRule().getStationRadius();
                     int diameterInPixels = (radius * 2 + 1) * WorldConstants.TILE_SIZE;
-                    int radiusX = (x - radius) * WorldConstants.TILE_SIZE;
-                    int radiusY = (y - radius) * WorldConstants.TILE_SIZE;
+                    int radiusX = (location.x - radius) * WorldConstants.TILE_SIZE;
+                    int radiusY = (location.y - radius) * WorldConstants.TILE_SIZE;
                     g.setColor(Color.WHITE);
                     g.setStroke(dashed);
                     g.draw(new RoundRectangle2D.Double(radiusX, radiusY, diameterInPixels, diameterInPixels, 10, 10));
@@ -117,24 +98,24 @@ public class StationNamesRenderer implements Painter {
 
                 // Then draw the station name.
                 if (showStationNames) {
-                    stationName = tempStation.getStationName();
+                    String stationName = station.getStationName();
 
-                    positionX = xdisp + WorldConstants.TILE_SIZE / 2;
-                    positionY = ydisp + WorldConstants.TILE_SIZE;
+                    int positionX = displayLocation.x + WorldConstants.TILE_SIZE / 2;
+                    int positionY = displayLocation.y + WorldConstants.TILE_SIZE;
 
-                    layout = new TextLayout(stationName, font, frc);
-                    visibleAdvance = layout.getVisibleAdvance();
+                    TextLayout layout = new TextLayout(stationName, font, g.getFontRenderContext());
+                    float visibleAdvance = layout.getVisibleAdvance();
 
-                    rectWidth = (int) (visibleAdvance * 1.2);
-                    rectHeight = (int) (fontSize * 1.5);
-                    rectX = (positionX - (rectWidth / 2));
-                    rectY = positionY;
+                    int rectWidth = (int) (visibleAdvance * 1.2);
+                    int rectHeight = (int) (fontSize * 1.5);
+                    int rectX = (positionX - (rectWidth / 2));
+                    int rectY = positionY;
 
                     g.setColor(bgColor);
                     g.fillRect(rectX, rectY, rectWidth, rectHeight);
 
-                    textX = (positionX - (visibleAdvance / 2));
-                    textY = positionY + fontSize + 1;
+                    float textX = (positionX - (visibleAdvance / 2));
+                    float textY = positionY + fontSize + 1;
 
                     g.setColor(textColor);
                     layout.draw(g, textX, textY);

src/main/java/freerails/client/renderer/map/TerrainLayerRenderer.java
@@ -36,7 +36,7 @@ public final class TerrainLayerRenderer implements MapLayerRenderer {
      * @param tileLocation
      */
     public void paintTile(Graphics g, Vector2D tileLocation) {
-        Vector2D screenLocation = Vector2D.multiply(ClientConfig.tileSize, tileLocation);
+        Vector2D screenLocation = Vector2D.multiply(ClientConfig.TILE_SIZE, tileLocation);
 
         if ((tileLocation.x >= 0) && (tileLocation.x < mapSize.x) && (tileLocation.y >= 0) && (tileLocation.y < mapSize.y)) {
             TerrainTile terrainTile = (TerrainTile) world.getTile(tileLocation);

src/main/java/freerails/client/renderer/map/detail/DetailMapRenderer.java
@@ -54,7 +54,7 @@ public class DetailMapRenderer implements MapRenderer {
         background = new SquareTileBackgroundRenderer(render);
 
         Vector2D mapSize = world.getMapSize();
-        mapSizeInPixels = Vector2D.multiply(mapSize, ClientConfig.tileSize);
+        mapSizeInPixels = Vector2D.multiply(mapSize, ClientConfig.TILE_SIZE);
 
         stationRadius = new StationRadiusRenderer(modelRoot);
         buildTrackRenderer = new BuildTrackRenderer(rendererRoot, modelRoot);

src/main/java/freerails/client/renderer/map/detail/StationBoxRenderer.java
@@ -44,32 +44,30 @@ import java.io.IOException;
 public class StationBoxRenderer implements Painter {
 
     private final ReadOnlyWorld world;
-    private final Color bgColor;
+    private static final Color BACKGROUND_COLOR = new Color(0, 0, 200, 60);;
     private final int wagonImageWidth;
     private final ModelRoot modelRoot;
     private final Image[] cargoImages;
 
     /**
      * @param world
-     * @param vl
+     * @param rendererRoot
      * @param modelRoot
      */
-    public StationBoxRenderer(ReadOnlyWorld world, RendererRoot vl, ModelRoot modelRoot) {
+    public StationBoxRenderer(ReadOnlyWorld world, RendererRoot rendererRoot, ModelRoot modelRoot) {
         this.world = world;
-        bgColor = new Color(0, 0, 200, 60);
         this.modelRoot = modelRoot;
 
-        // How wide will the wagon images be if we scale them so their height is
-        // WAGON_IMAGE_HEIGHT?
-        Image wagonImage = vl.getWagonImages(0).getSideOnImage();
+        // How wide will the wagon images be if we scale them so their height is WAGON_IMAGE_HEIGHT?
+        Image wagonImage = rendererRoot.getWagonImages(0).getSideOnImage();
         wagonImageWidth = wagonImage.getWidth(null) * ClientConfig.WAGON_IMAGE_HEIGHT / wagonImage.getHeight(null);
 
         int nrOfCargoTypes = this.world.size(SharedKey.CargoTypes);
         cargoImages = new Image[nrOfCargoTypes];
         for (int i = 0; i < nrOfCargoTypes; i++) {
-            String wagonFilename = vl.getWagonImages(i).sideOnFileName;
+            String wagonFilename = rendererRoot.getWagonImages(i).sideOnFileName;
             try {
-                wagonImage = vl.getScaledImage(wagonFilename, ClientConfig.WAGON_IMAGE_HEIGHT);
+                wagonImage = rendererRoot.getScaledImage(wagonFilename, ClientConfig.WAGON_IMAGE_HEIGHT);
             } catch (IOException e) {
                 throw new IllegalArgumentException(wagonFilename);
             }
@@ -87,22 +85,24 @@ public class StationBoxRenderer implements Painter {
         if (showCargoWaiting) {
             // We only show the station boxes for the current player.
             FreerailsPrincipal principal = modelRoot.getPrincipal();
-            WorldIterator wi = new NonNullElementWorldIterator(PlayerKey.Stations, world, principal);
+            WorldIterator worldIterator = new NonNullElementWorldIterator(PlayerKey.Stations, world, principal);
 
-            while (wi.next()) { // loop over non null stations
-                Station station = (Station) wi.getElement();
-                int positionX = (station.getStationP().x * WorldConstants.TILE_SIZE) + WorldConstants.TILE_SIZE / 2;
-                int positionY = (station.getStationP().y * WorldConstants.TILE_SIZE) + WorldConstants.TILE_SIZE * 2;
+            // TODO can there be null stations?
+            while (worldIterator.next()) { // loop over non null stations
+                Station station = (Station) worldIterator.getElement();
+                // TODO which position is meant here?
+                int positionX = (station.getLocation().x * WorldConstants.TILE_SIZE) + WorldConstants.TILE_SIZE / 2;
+                int positionY = (station.getLocation().y * WorldConstants.TILE_SIZE) + WorldConstants.TILE_SIZE * 2;
                 Rectangle r = new Rectangle(positionX, positionY, ClientConfig.MAX_WIDTH, ClientConfig.MAX_HEIGHT);
                 if (newVisibleRectangle.intersects(r)) {
-                    g.setColor(bgColor);
-                    g.fillRect(positionX, positionY, ClientConfig.MAX_WIDTH, ClientConfig.MAX_HEIGHT);
+                    g.setColor(StationBoxRenderer.BACKGROUND_COLOR);
+                    g.fillRect(r.x, r.y, r.width, r.height);
                     g.setColor(Color.WHITE);
                     g.setStroke(new BasicStroke(1.0f));
-                    g.drawRect(positionX, positionY, ClientConfig.MAX_WIDTH, ClientConfig.MAX_HEIGHT);
+                    g.drawRect(r.x, r.y, r.width, r.height);
 
-                    CargoBatchBundle cb = (ImmutableCargoBatchBundle) world.get(principal, PlayerKey.CargoBundles, station.getCargoBundleID());
-                    int[][] carsLoads = calculateCarLoads(cb);
+                    CargoBatchBundle cargoBatchBundle = (ImmutableCargoBatchBundle) world.get(principal, PlayerKey.CargoBundles, station.getCargoBundleID());
+                    int[][] carsLoads = calculateCarLoads(cargoBatchBundle);
                     for (int category = 0; category < CargoCategory.getNumberOfCategories(); category++) {
                         int alternateWidth = (ClientConfig.MAX_WIDTH - 2 * ClientConfig.SPACING) / (carsLoads[category].length + 1);
                         int xOffsetPerWagon = Math.min(wagonImageWidth, alternateWidth);
@@ -119,20 +119,21 @@ public class StationBoxRenderer implements Painter {
         }
     }
 
+    // TODO move this to cargo batch bundle maybe?
     /**
      * The length of the returned array is the number of complete carloads of
      * the specified cargo category in the specified bundle. The values in the
      * array are the type of the cargo. E.g. if the bundle contained 2 carloads
      * of cargo type 3 and 1 of type 7, {3, 3, 7} would be returned.
      */
-    private int[][] calculateCarLoads(CargoBatchBundle cb) {
+    private int[][] calculateCarLoads(CargoBatchBundle cargoBatchBundle) {
         int categories = CargoCategory.getNumberOfCategories();
         int numCargoTypes = world.size(SharedKey.CargoTypes);
         int[] numberOfCarLoads = new int[categories];
         int[][] cars = new int[categories][numCargoTypes];
         for (int i = 0; i < numCargoTypes; i++) {
             CargoType ct = (CargoType) world.get(SharedKey.CargoTypes, i);
-            int carsOfThisCargo = cb.getAmountOfType(i) / WagonType.UNITS_OF_CARGO_PER_WAGON;
+            int carsOfThisCargo = cargoBatchBundle.getAmountOfType(i) / WagonType.UNITS_OF_CARGO_PER_WAGON;
             numberOfCarLoads[ct.getCategory().getID()] += carsOfThisCargo;
             cars[ct.getCategory().getID()][i] += carsOfThisCargo;
         }

src/main/java/freerails/client/renderer/track/BuildTrackRenderer.java
@@ -69,7 +69,7 @@ public class BuildTrackRenderer implements Painter {
 
                 int ruleNumber = trackPiece.getTrackTypeID();
                 TrackPieceRenderer trackPieceView = rendererRoot.getTrackPieceView(ruleNumber);
-                trackPieceView.drawTrackPieceIcon(g, graphicsNumber, point, ClientConfig.tileSize);
+                trackPieceView.drawTrackPieceIcon(g, graphicsNumber, point, ClientConfig.TILE_SIZE);
             }
 
             ReadOnlyWorld realWorld = modelRoot.getWorld();
@@ -81,8 +81,8 @@ public class BuildTrackRenderer implements Painter {
             for (Iterator<Vector2D> iter = worldDiffs.getMapDiffs(); iter.hasNext(); ) {
                 Vector2D p = iter.next();
                 // TODO replace by Vector2D arithmetics
-                int x = p.x * ClientConfig.tileSize.x + (ClientConfig.tileSize.x - ClientConfig.SMALL_DOT_WIDTH) / 2;
-                int y = p.y * ClientConfig.tileSize.y + (ClientConfig.tileSize.y - ClientConfig.SMALL_DOT_WIDTH) / 2;
+                int x = p.x * ClientConfig.TILE_SIZE.x + (ClientConfig.TILE_SIZE.x - ClientConfig.SMALL_DOT_WIDTH) / 2;
+                int y = p.y * ClientConfig.TILE_SIZE.y + (ClientConfig.TILE_SIZE.y - ClientConfig.SMALL_DOT_WIDTH) / 2;
                 FullTerrainTile before = (FullTerrainTile) realWorld.getTile(p);
                 FullTerrainTile after = (FullTerrainTile) worldDiffs.getTile(p);
 

src/main/java/freerails/client/renderer/track/TrackLayerRenderer.java
@@ -3,7 +3,6 @@ package freerails.client.renderer.track;
 import freerails.client.ClientConfig;
 import freerails.client.renderer.RendererRoot;
 import freerails.client.renderer.map.MapLayerRenderer;
-import freerails.client.renderer.track.TrackPieceRenderer;
 import freerails.model.terrain.FullTerrainTile;
 import freerails.model.track.NullTrackType;
 import freerails.model.track.TrackPiece;
@@ -56,7 +55,7 @@ public final class TrackLayerRenderer implements MapLayerRenderer {
                     int ruleNumber = trackPiece.getTrackTypeID();
                     if (ruleNumber != NullTrackType.NULL_TRACK_TYPE_RULE_NUMBER) {
                         TrackPieceRenderer trackPieceView = rendererRoot.getTrackPieceView(ruleNumber);
-                        trackPieceView.drawTrackPieceIcon(g, graphicsNumber, tileLocation, ClientConfig.tileSize);
+                        trackPieceView.drawTrackPieceIcon(g, graphicsNumber, tileLocation, ClientConfig.TILE_SIZE);
                     }
                 }
             }

src/main/java/freerails/client/view/StationInfoPanel.java
@@ -161,7 +161,7 @@ public class StationInfoPanel extends JPanel implements View, WorldListListener 
 
         // Add your handling code here:
         if (worldIterator.previous()) {
-            Vector2D p = ((Station) worldIterator.getElement()).getStationP();
+            Vector2D p = ((Station) worldIterator.getElement()).getLocation();
             modelRoot.setProperty(ModelRootProperty.CURSOR_POSITION, p);
 
             display();
@@ -174,7 +174,7 @@ public class StationInfoPanel extends JPanel implements View, WorldListListener 
 
         // Add your handling code here:
         if (worldIterator.next()) {
-            Vector2D p = ((Station) worldIterator.getElement()).getStationP();
+            Vector2D p = ((Station) worldIterator.getElement()).getLocation();
             modelRoot.setProperty(ModelRootProperty.CURSOR_POSITION, p);
             display();
         } else {

src/main/java/freerails/model/station/Station.java
@@ -211,7 +211,7 @@ public class Station implements Serializable {
     /**
      * @return
      */
-    public Vector2D getStationP() {
+    public Vector2D getLocation() {
         return location;
     }
 

src/main/java/freerails/move/receiver/MoveChainFork.java
@@ -88,14 +88,9 @@ public class MoveChainFork implements MoveReceiver {
         for (MoveReceiver moveReceiver : moveReceivers) {
             moveReceiver.process(move);
         }
-        // CompositeMoves are splitted here
-        if (move instanceof CompositeMove) {
-            for (Move subMove : ((CompositeMove) move).getMoves()) {
-                splitMove(subMove);
-            }
-        } else {
-            splitMove(move);
-        }
+
+        // split the move and process it
+        splitMove(move);
     }
 
     /**
@@ -105,36 +100,44 @@ public class MoveChainFork implements MoveReceiver {
      */
     private void splitMove(Move move) {
 
-        // all split move receivers process the move
-        for (MoveReceiver moveReceiver : splitMoveReceivers) {
-            moveReceiver.process(move);
-        }
+        // CompositeMoves are splitted here
+        if (move instanceof CompositeMove) {
+            for (Move subMove : ((CompositeMove) move).getMoves()) {
+                splitMove(subMove);
+            }
+        } else {
 
-        if (move instanceof AddItemToListMove) {
-            ListMove listMove = (AddItemToListMove) move;
-            for (WorldListListener listener : listListeners) {
-                listener.itemAdded(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
+            // all split move receivers process the move
+            for (MoveReceiver moveReceiver : splitMoveReceivers) {
+                moveReceiver.process(move);
             }
-        } else if (move instanceof ChangeItemInListMove) {
-            ListMove listMove = (ChangeItemInListMove) move;
-            for (WorldListListener listener : listListeners) {
-                listener.listUpdated(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
-            }
-        } else if (move instanceof RemoveItemFromListMove) {
-            ListMove listMove = (RemoveItemFromListMove) move;
-            for (WorldListListener listener : listListeners) {
-                listener.itemRemoved(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
-            }
-        } else if (move instanceof MapUpdateMove) {
-            Rectangle rectangle = ((MapUpdateMove) move).getUpdatedTiles();
-            // TODO can r be 0,0,0,0
-            if (rectangle.x != 0 && rectangle.y != 0 && rectangle.width != 0 && rectangle.height != 0) {
-                for (WorldMapListener listener : mapListeners) {
-                    listener.tilesChanged(rectangle);
+
+            if (move instanceof AddItemToListMove) {
+                ListMove listMove = (AddItemToListMove) move;
+                for (WorldListListener listener : listListeners) {
+                    listener.itemAdded(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
                 }
+            } else if (move instanceof ChangeItemInListMove) {
+                ListMove listMove = (ChangeItemInListMove) move;
+                for (WorldListListener listener : listListeners) {
+                    listener.listUpdated(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
+                }
+            } else if (move instanceof RemoveItemFromListMove) {
+                ListMove listMove = (RemoveItemFromListMove) move;
+                for (WorldListListener listener : listListeners) {
+                    listener.itemRemoved(listMove.getKey(), listMove.getIndex(), listMove.getPrincipal());
+                }
+            } else if (move instanceof MapUpdateMove) {
+                Rectangle rectangle = ((MapUpdateMove) move).getUpdatedTiles();
+                // TODO can r be 0,0,0,0
+                if (rectangle.x != 0 && rectangle.y != 0 && rectangle.width != 0 && rectangle.height != 0) {
+                    for (WorldMapListener listener : mapListeners) {
+                        listener.tilesChanged(rectangle);
+                    }
+                }
+            } else if (move instanceof TimeTickMove) {
+                lastTickTime = System.currentTimeMillis();
             }
-        } else if (move instanceof TimeTickMove) {
-            lastTickTime = System.currentTimeMillis();
         }
     }
 

src/main/java/freerails/util/Vector2D.java
@@ -90,14 +90,14 @@ public final class Vector2D implements Serializable, Comparable<Vector2D> {
         return new Vector2D(a.x * b.x, a.y * b.y);
     }
 
+    public static Vector2D multiply(Vector2D a, int b) {
+        return new Vector2D(a.x * b, a.y * b);
+    }
+
 
     public int compareTo(Vector2D o) {
         if (o.y != y) return y - o.y;
         else return x - o.x;
-    }
-
-    public static Vector2D fromPoint(Point p) {
-        return new Vector2D(p.x, p.y);
     }
 
     public static Point toPoint(Vector2D p) {